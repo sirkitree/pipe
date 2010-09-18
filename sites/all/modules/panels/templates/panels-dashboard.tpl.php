8a69a51c316df20c415b4a48e496db0acebfe306
sirkitree <sirkitree@gmail.com>
2010-09-18 16:47:31 -0400

    pipe repo - panels everywhere and in-place editor with fusion theme

---
+++ sites/all/modules/panels/templates/panels-dashboard.tpl.php
@@ -0,0 +1,12 @@
+<?php
+// $Id$
+?>
+<div class="panels-dashboard">
+  <div class="dashboard-left">
+    <?php print $left; ?>
+  </div>
+
+  <div class="dashboard-right">
+    <?php print $right; ?>
+  </div>
+</div>
